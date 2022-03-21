a7ad6f1b5d2cbc8996c95ae81a460be01fb06625
dversoza <daniel.ve3@gmail.com>
2022-03-21 12:26:52 -0300

    Created tags model and enpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@unittest.com', password='password'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(
+        email=email,
+        password=password
+    )
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
@@ -45,3 +55,12 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.is_superuser, True)
         self.assertEqual(user.is_staff, True)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Healthy'
+        )
+
+        self.assertEqual(str(tag), tag.name)
