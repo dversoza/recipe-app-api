516aadcc1b0f9c6d0c0ec29761d47dd43e914129
dversoza <daniel.ve3@gmail.com>
2022-03-21 23:37:28 -0300

    Added recipe image upload feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,15 +1,12 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
 from core.models import Tag, Ingredient, Recipe
 
-from recipe.serializers import (
-    TagSerializer,
-    IngredientSerializer,
-    RecipeSerializer,
-    RecipeDetailSerializer
-)
+from recipe import serializers
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
@@ -40,18 +37,18 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 class TagViewSet(BaseRecipeAttrViewSet):
     """Manage tags in the database"""
     queryset = Tag.objects.all()
-    serializer_class = TagSerializer
+    serializer_class = serializers.TagSerializer
 
 
 class IngredientsViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
     queryset = Ingredient.objects.all()
-    serializer_class = IngredientSerializer
+    serializer_class = serializers.IngredientSerializer
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipes in the database"""
-    serializer_class = RecipeSerializer
+    serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -63,7 +60,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
-            return RecipeDetailSerializer
+            return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -74,3 +73,31 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         """Update a recipe object"""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    @action(methods=['POST'], detail=True, url_path='delete-image')
+    def delete_image(self, request, pk=None):
+        """Delete an image from a recipe"""
+        recipe = self.get_object()
+        recipe.image.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
